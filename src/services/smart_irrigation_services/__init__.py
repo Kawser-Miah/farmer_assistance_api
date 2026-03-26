8203f3f76dc00bbbb53b6395d23e289acf81c255
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-03-26 18:52:39 +0600

    Add initialization for smart irrigation services module

---
+++ src/services/smart_irrigation_services/__init__.py
@@ -0,0 +1,4 @@
+from src.services.smart_irrigation_services.model import get_irrigation_model
+from src.services.smart_irrigation_services.predict import predict_irrigation
+
+__all__ = ["get_irrigation_model", "predict_irrigation"]
